6588bc24e822456b2a7ad6b53fb5191d1469d571
Connor Ring <crv9557@saintjoe.edu>
2015-02-04 04:55:51 -0500

    Revision for RangeSum.js

--- RangeSum.js
+++ RangeSum.js
@@ -1,8 +1,14 @@
+// Connor Ring RangeSum Code for Ch. 4
+// February 3rd, 2015
+// RangeSum.js
+
+// The program is supposed to add elements in an array and send them out to the console.
+
 // Setup the array for start and end
 function range (start, end) {
   var array = [];
-  for (var i = start; i <= end)
-	array push(i);
+  for (var i = start; i <= end; i++)
+	array.push(i);
   return array;
 }
 
@@ -10,7 +16,7 @@ function range (start, end) {
 function sum(array) {
     var total = 0;
     for (var i = 0; i < array.length; i++)
-      total += array[i]:
+      total += array[i];
     return total;
 }
 
